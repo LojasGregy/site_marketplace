63b4de7c82e6ce0434fa4772a69e9c02fa81a329
Lojas Gregy <108229578+LojasGregy@users.noreply.github.com>
2023-03-03 23:34:46 -0300

    Create config-sample.php

---
+++ config-sample.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base Mysql settings of Lojas Gregy
+ */
+define('MULTISITE',0);
+
+/**MySQL database name for Lojas Gregy */
+define('DB_name','database_name');
+
+/**MySQL database username */
+define('DB_USER','username');
+
+/**MySQL database password */
+define('DB_PASSWORD','password');
+
+/**MySQL hostname */
+define('DB_HOST','localhost');
+
+/**Database Table prefix */
+define('DB_TABLE_PREFIX','oc_');
+
+define('REL_WEB_URL','rel_here');
+
+define('WEB_PATH','https//localhost');
+
+?>
